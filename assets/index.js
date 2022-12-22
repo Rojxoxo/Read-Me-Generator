4d9da9540a2d92707d06e07ac62ebe7fcbd1596c
RogLoL <115582357+Rojxoxo@users.noreply.github.com>
2022-12-21 21:37:03 -0600

    added questions that can be answered in the command line and recorded user input onto readme file

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,60 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs"); 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "A description of your project:",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Installation instructions for your project:",
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "Enter usage information:",
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Enter contribution guidlines:",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Enter test instructions:",
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Enter your gitHub username:",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email:",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+
+inquirer.prompt(questions).then((answers) => {
+    fs.writeFile('README.md', JSON.stringify(answers), (error) => {
+        return error    
+            ? console.error(error)
+            : console.log('File written successfully')
+    })
+});
 
 // TODO: Create a function to initialize app
 function init() {}
